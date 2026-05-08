abe201153737e1bc6c81dd2ec7393414afc4fc4c
MozzyMchanz <bmoses1@byupathway.edu>
2026-05-08 07:33:03 -0700

    cm

---
+++ scripts/getdates.js
@@ -0,0 +1,3 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('copyright').textContent = `\u00A9 ${currentYear} BWIRE MOSES \u2022 Kampala, Uganda.`;
+document.getElementById('lastModified').textContent = `Last Modification: ${document.lastModified}`;
